585a2b655f255ec04490c95772d00b1c3185bbbf
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-17 10:35:47 +0900

    [feat/#4] 대시보드 버튼

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+
 type LoginButtonPros = {
   disabled: boolean;
 };
@@ -5,10 +7,31 @@ type LoginButtonPros = {
 export function LoginButton({ disabled }: LoginButtonPros) {
   return (
     <button
-      className="bg-violet_5534DA disabled:bg-gray_9FA6B2 flex justify-center items-center text-2lg text-white_FFFFFF radius-[8px] w-full py-[12px] md:py-[14px]"
+      className="bg-violet_5534DA disabled:bg-gray_9FA6B2 flex justify-center items-center text-2lg text-white_FFFFFF rounded-[8px] w-full py-[12px] md:py-[14px]"
       disabled={disabled}
     >
       로그인
     </button>
   );
 }
+
+type DashboardButtonProps = {
+  mode: "accept" | "deny";
+};
+
+export function DashboardButton({ mode }: DashboardButtonProps) {
+  const isAccept = mode === "accept";
+
+  return (
+    <button
+      className={`rounded-[4px] py-[7px] px-[44px] text-xs md:px-[23px] md:py-[6px] md:text-md lg:px-[29px] lg:py-[7px]  ${
+        isAccept
+          ? "bg-violet_5534DA text-white_FFFFFF"
+          : "bg-white_FFFFFF text-violet_5534DA border border-gray_D9D9D9"
+      }`}
+    >
+      {mode === "accept" ? "수락" : "거절"}
+    </button>
+  );
+}
+
