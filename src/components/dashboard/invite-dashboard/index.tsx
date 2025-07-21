94a00885c0aa3074ff11bb56ffcda14a0c07a686
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-21 23:05:14 +0900

    [feat/#35] 초대받은 대시보드

--- src/components/dashboard/invite-dashboard/index.tsx
+++ src/components/dashboard/invite-dashboard/index.tsx
@@ -41,9 +41,9 @@ export function InviteDashboard() {
   ];
 
   return (
-    <div className="px-[24px] py-[16px]">
-      <div className="flex flex-col gap-y-[16px]">
-        <h1 className="text-xl text-black-333236 font-bold">
+    <div className="px-[24px] py-[16px] md:px-[28px] md:py-[18px] lg:py-[32px] md:px-[28px]">
+      <div className="flex flex-col gap-y-[16px] mb-[13px] md:mb-[24px] lg:gap-y-[32px]">
+        <h1 className="text-xl text-black-333236 font-bold md:text-2xl">
           초대받은 대시보드
         </h1>
         <div className="relative">
@@ -58,10 +58,34 @@ export function InviteDashboard() {
           />
         </div>
       </div>
-      <div className="flex flex-col">
+
+      <div className="flex flex-col md:hidden">
         {exampleList.map(({ name, invite }) => (
           <MobileInviteList name={name} invite={invite} />
         ))}
+      </div>
+
+      <div className="hidden md:flex md:flex-col md:gap-y-[17px]">
+        <div className="grid grid-cols-[2fr_2fr_3fr] items-center justify-items-center">
+          <span className="text-lg text-gray_9FA6B2">이름</span>
+          <span className="text-lg text-gray_9FA6B2">초대자</span>
+          <span className="text-lg text-gray_9FA6B2">수락 여부</span>
+        </div>
+        <div className="flex flex-col">
+          {exampleList.map(({ name, invite }) => (
+            <div
+              key={name}
+              className="grid grid-cols-[2fr_2fr_3fr] items-center justify-items-center border-b border-gray_EEEEEE py-[22px] lg:py-[20px]"
+            >
+              <span className="text-lg text-black_333236">{name}</span>
+              <span className="text-lg text-black_333236">{invite}</span>
+              <div className="flex gap-x-[10px]">
+                <DashboardButton mode="accept" />
+                <DashboardButton mode="deny" />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
