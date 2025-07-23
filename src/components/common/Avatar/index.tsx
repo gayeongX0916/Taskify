a179110bcdc562da743b54a3c960d71b15023a15
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-23 16:16:15 +0900

    [feat/#47] 랜덤 프로필 만들기

---
+++ src/components/common/Avatar/index.tsx
@@ -0,0 +1,18 @@
+import { getInitialFromUserName, getRandomColor } from "@/utils/randomColor";
+
+type AvatarProps = {
+  username: string;
+};
+
+export function Avatar({ username }: AvatarProps) {
+  const initial = getInitialFromUserName(username);
+  const bgColor = getRandomColor(initial);
+
+  return (
+    <div
+      className={`rounded-full flex items-center justify-center text-white text-md md:text-lg bg-[${bgColor}] w-[34px] h-[34px] font-semibold border-2 border-white_FFFFFF md:w-[38px] md:h-[38px]`}
+    >
+      {initial}
+    </div>
+  );
+}
---
+++ src/utils/randomColor.ts
@@ -0,0 +1,22 @@
+export const pastelColors = [
+  "#A7D7C5",
+  "#9FC5E8",
+  "#D6A2E8",
+  "#FFB3B3",
+  "#FFCC99",
+  "#AED581",
+  "#81D4FA",
+  "#B39DDB",
+];
+
+export function getRandomColor(char: string) {
+  const code = char.charCodeAt(0);
+  const index = code % pastelColors.length;
+  return pastelColors[index];
+}
+
+export function getInitialFromUserName(username: string) {
+  const trimmed = username.trim();
+  const firstChar = trimmed[0];
+  return firstChar;
+}
