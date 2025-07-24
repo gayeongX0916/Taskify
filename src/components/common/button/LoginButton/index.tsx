e211a9135e2df2adcbdd4edf9f6c45bc35bd2415
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-24 22:57:14 +0900

    [refactor/#42] 대시보드 버튼 수정

--- src/components/common/button/LoginButton/index.tsx
+++ src/components/common/button/LoginButton/index.tsx
@@ -8,7 +8,7 @@ type LoginButtonPros = {
 export function LoginButton({ disabled, children }: LoginButtonPros) {
   return (
     <button
-      className="bg-violet_5534DA disabled:bg-gray_9FA6B2 flex justify-center items-center text-2lg text-white_FFFFFF rounded-[8px] w-full py-[12px] md:py-[14px]"
+      className="bg-violet_5534DA disabled:bg-gray_9FA6B2 flex justify-center items-center text-2lg text-white_FFFFFF rounded-[8px] w-full py-[14px]"
       disabled={disabled}
     >
       {children}
