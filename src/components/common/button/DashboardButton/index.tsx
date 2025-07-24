f4a9676f450e9ec50bb1c0fee1267629a25d6660
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-24 23:11:15 +0900

    [refactor/#42] onClick 속성 추가

--- src/components/common/button/DashboardButton/index.tsx
+++ src/components/common/button/DashboardButton/index.tsx
@@ -1,13 +1,15 @@
 type DashboardButtonProps = {
   mode: "accept" | "deny";
+  onClick?: () => void;
 };
 
-export function DashboardButton({ mode }: DashboardButtonProps) {
+export function DashboardButton({ mode, onClick }: DashboardButtonProps) {
   const isAccept = mode === "accept";
 
   return (
     <button
-      className={`flex justify-center items-center rounded-[4px] py-[7px] px-[37px] text-xs md:px-[23px] md:py-[6px] md:text-md lg:px-[29px] lg:py-[7px]  ${
+      onClick={onClick}
+      className={`flex justify-center items-center rounded-[4px] py-[7px] px-[37px] w-full text-xs md:w-[auto] md:px-[23px] md:py-[6px] md:text-md lg:px-[29px] lg:py-[7px]  ${
         isAccept
           ? "bg-violet_5534DA text-white_FFFFFF"
           : "bg-white_FFFFFF text-violet_5534DA border border-gray_D9D9D9"
---
+++ src/components/common/button/ModalButton/index.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+
+type ModalButtonProps = {
+  mode: "any" | "cancel";
+  children: ReactNode;
+  onClick?: () => void;
+};
+
+export function ModalButton({ mode, children, onClick }: ModalButtonProps) {
+  const isCancel = mode === "cancel";
+
+  return (
+    <button
+      onClick={onClick}
+      className={`rounded-[8px] flex justify-center items-center px-[46px] py-[14px] text-lg w-full ${
+        isCancel
+          ? "text-gray_787486 border border-gray_D9D9D9"
+          : "bg-violet_5534DA text-white_FFFFFF"
+      }`}
+    >
+      {mode === "cancel" ? "취소" : children}
+    </button>
+  );
+}
