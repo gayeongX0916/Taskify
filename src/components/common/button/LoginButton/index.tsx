5075c2d7eaa807f0b9148b6b5e6e2e89311d043d
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-24 22:46:09 +0900

    [refator/#42] 로그인 버튼 수정

---
+++ src/components/common/button/LoginButton/index.tsx
@@ -0,0 +1,17 @@
+import { Children, ReactNode } from "react";
+
+type LoginButtonPros = {
+  disabled: boolean;
+  children: ReactNode;
+};
+
+export function LoginButton({ disabled, children }: LoginButtonPros) {
+  return (
+    <button
+      className="bg-violet_5534DA disabled:bg-gray_9FA6B2 flex justify-center items-center text-2lg text-white_FFFFFF rounded-[8px] w-full py-[12px] md:py-[14px]"
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
