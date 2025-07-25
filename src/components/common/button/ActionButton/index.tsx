4eb2928aadf1642a3f33f038c3c99a63de1257be
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-25 16:39:24 +0900

    [refactor/#43] action button 추가

---
+++ src/components/common/button/ActionButton/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+type ActionButtonProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+export function ActionButton({ children, className }: ActionButtonProps) {
+  return (
+    <button
+      className={`border border-gray_D9D9D9 rounded-[4px] px-[15px] py-[4px] text-xs text-violet_5534DA md:px-[29px] md:py-[7px] ${
+        className ?? ""
+      }`}
+    >
+      {children}
+    </button>
+  );
+}
