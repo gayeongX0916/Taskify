47b72f2bdfefef716fb7c97bafcb25d18c520a18
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-25 23:10:17 +0900

    [refactor/#43] input 구조 변경

--- src/components/modal/create-todo/index.tsx
+++ src/components/modal/create-todo/index.tsx
@@ -6,12 +6,29 @@ import { useState } from "react";
 import { ModalButton } from "@/components/common/Button/ModalButton";
 import { BaseInput } from "@/components/common/Input/BaseInput";
 import { TextareaInput } from "@/components/common/Input/ModalInput/TextareaInput";
-import { DateInput } from "@/components/common/Input/ModalInput/dateInput";
+import { DateInput } from "@/components/common/Input/ModalInput/DateInput";
 import { TagInput } from "@/components/common/Input/ModalInput/TagInput";
 import { ImageInput } from "@/components/common/Input/ModalInput/ImageInput";
 
+type ModalValues = {
+  assignee: string;
+  title: string;
+  des: string;
+  date: Date | null;
+  tag: string[];
+  img: string;
+};
+
 export function ModalCreateTodo() {
   const [isOpen, setIsOpen] = useState(false);
+  const [values, setValues] = useState<ModalValues>({
+    assignee: "",
+    title: "",
+    des: "",
+    date: null,
+    tag: [],
+    img: "",
+  });
 
   return (
     <Dialog open={true} onClose={setIsOpen}>
@@ -26,13 +43,30 @@ export function ModalCreateTodo() {
                 <span className="text-lg text-black_333236">담당자</span>
                 <AssigneeDropdown />
               </div>
-              <BaseInput label="제목 *" placeholder="제목을 입력해 주세요" />
+              <BaseInput
+                label="제목 *"
+                placeholder="제목을 입력해 주세요"
+                value={values.title}
+                onChange={(value) => setValues({ ...values, title: value })}
+              />
               <TextareaInput
                 label="설명 *"
                 placeholder="설명을 입력해 주세요"
+                value={values.des}
+                onChange={(value) => setValues({ ...values, des: value })}
               />
-              <DateInput label="마감일 *" />
-              <TagInput label="태그" placeholder="입력 후 Enter" />
+              <DateInput
+                label="마감일 *"
+                placeholder="날짜를 입력해 주세요"
+                value={values.date}
+                onChange={(value) => setValues({ ...values, date: value })}
+              />
+              <TagInput
+                label="태그"
+                placeholder="입력 후 Enter"
+                value={values.tag}
+                onChange={(value) => setValues({ ...values, tag: value })}
+              />
               <ImageInput label="이미지" />
 
               <div className="flex gap-x-[11px]">
