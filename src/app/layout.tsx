0d33311eaa033d56039a90ce358e9639f0d63e0e
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-26 22:37:44 +0900

    [refactor/#44] sidemenu 구조 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
+import { SideMenu } from "@/components/sidemenu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
+        <SideMenu />
+        <div className="pl-[67px] md:pl-[160px] lg:pl-[300px]">
+          <Header />
+          <main>{children}</main>
+        </div>
       </body>
     </html>
   );
