d3f0e98c4c4c699bdd903d18a0e24bb8da479d45
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-27 23:44:39 +0900

    [refactor/#52] Avatar 수정

--- src/components/common/Avatar/index.tsx
+++ src/components/common/Avatar/index.tsx
@@ -2,17 +2,19 @@ import { getInitialFromUserName, getRandomColor } from "@/utils/randomColor";
 
 type AvatarProps = {
   username: string;
+  className?: string;
 };
 
-export function Avatar({ username }: AvatarProps) {
+export function Avatar({ username, className }: AvatarProps) {
   const initial = getInitialFromUserName(username);
   const bgColor = getRandomColor(initial);
 
   return (
     <div
-      className={
-        "rounded-full flex items-center justify-center text-white text-md md:text-lg w-[34px] h-[34px] font-semibold border-2 border-white_FFFFFF md:w-[38px] md:h-[38px]"
-      }
+      className={`rounded-full flex items-center justify-center text-white font-medium border-2 border-white_FFFFFF ${
+        className ??
+        "w-[34px] h-[34px] md:w-[38px] md:h-[38px] text-md md:text-lg"
+      }`}
       style={{ backgroundColor: bgColor }}
     >
       {initial}
