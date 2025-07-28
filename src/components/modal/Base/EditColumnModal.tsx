ea65c46de81393214657e050bb1dbc4f39d23401
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-28 22:38:37 +0900

    [refactor/#53] EditModal 수정

---
+++ src/components/modal/Base/EditColumnModal.tsx
@@ -0,0 +1,20 @@
+import { ModalProps } from "@/types/ModalProps";
+import { BaseModal } from ".";
+import { ModalButton } from "@/components/common/Button/ModalButton";
+
+export function EditColumnModal({ isOpen, onClose }: ModalProps) {
+  return (
+    <BaseModal isOpen={isOpen} onClose={onClose} title="칼럼 관리">
+      <div className="flex flex-col gap-y-[8px] mb-[24px]">
+        <span className="text-lg text-black_333236">이름</span>
+        <input className="px-[16px] py-[15px] rounded-[8px] border border-gray_D9D9D9" />
+      </div>
+      <div className="flex gap-x-[7px]">
+        <ModalButton mode="cancel" onClick={onClose}>
+          삭제
+        </ModalButton>
+        <ModalButton mode="any">변경</ModalButton>
+      </div>
+    </BaseModal>
+  );
+}
