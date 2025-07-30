68056d14be0855f955ded9ad6b26d57103167991
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-30 21:40:47 +0900

    [feat/#67] 페이지 연결

--- src/components/common/gnb/DefaultHeader/index.tsx
+++ src/components/common/gnb/DefaultHeader/index.tsx
@@ -1,20 +1,27 @@
+"use client";
+
 import Image from "next/image";
 import logoWhite from "@/assets/logo_white.svg";
 import logoTitleWhite from "@/assets/logo_title_white.svg";
+import { useRouter } from "next/navigation";
 
 export default function DefaultHeader() {
+  const router = useRouter();
+
+  const handleGotoHome = () => router.push("/");
+
   return (
     <header className="bg-black_000000 flex justify-between items-center px-[24px] py-[16px] md:px-[40px] md:py-[15px] lg:px-[80px]">
-      <button className="flex md:hidden">
+      <button className="flex md:hidden" onClick={handleGotoHome}>
         <Image src={logoWhite} alt="로고" />
       </button>
-      <button className="hidden md:flex">
+      <button className="hidden md:flex" onClick={handleGotoHome}>
         <Image src={logoTitleWhite} alt="로고" width={121} height={39} />
       </button>
 
       <div className="flex gap-x-[24px] text-white_FFFFFF text-md md:text-lg md:gap-x-[36px]">
-        <button>로그인</button>
-        <button>회원가입</button>
+        <button onClick={() => router.push("/login")}>로그인</button>
+        <button onClick={() => router.push("/signup")}>회원가입</button>
       </div>
     </header>
   );
