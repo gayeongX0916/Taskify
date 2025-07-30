c5c3876c15f3cb8a6c83ae425473a19930c07287
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-30 12:45:04 +0900

    [refactor/#56] TagList 수정

--- src/components/common/TagList.tsx
+++ src/components/common/TagList.tsx
@@ -2,20 +2,46 @@ import { getTagColor } from "@/utils/getTagColor";
 
 type TagListProps = {
   tags: string[];
+  className?: string;
+  isButton?: boolean;
+  onClickTag?: (tag: string) => void;
 };
 
-export function TagList({ tags }: TagListProps) {
+export function TagList({
+  tags,
+  className,
+  isButton = false,
+  onClickTag,
+}: TagListProps) {
   return (
-    <ul className="flex items-center gap-x-[6px]">
+    <ul className={`flex items-center flex-wrap ${className ?? "gap-x-[6px]"}`}>
       {tags.map((tag) => {
         const color = getTagColor(tag);
+        const commonStyle = {
+          backgroundColor: color.bg,
+          color: color.text,
+        };
+
         return (
-          <li
-            key={tag}
-            className="rounded-[4px] px-[6px] py-[2px] text-md"
-            style={{ backgroundColor: color.bg, color: color.text }}
-          >
-            {tag}
+          <li key={tag}>
+            {isButton ? (
+              <button
+                key={tag}
+                onClick={() => onClickTag?.(tag)}
+                className="rounded-[4px] px-[6px] py-[2px] text-md"
+                style={commonStyle}
+              >
+                {tag}
+              </button>
+            ) : (
+              <span
+                key={tag}
+                className="rounded-[4px] px-[6px] py-[4px] text-md"
+                style={commonStyle}
+              >
+                {tag}
+              </span>
+            )}
           </li>
         );
       })}
