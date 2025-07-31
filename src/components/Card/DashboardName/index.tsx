267e3651c05037a10e9db42207f4d2979abee2d3
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-31 19:35:19 +0900

    [feat/#71] DashboardNameCard 수정

--- src/components/Card/DashboardName/index.tsx
+++ src/components/Card/DashboardName/index.tsx
@@ -1,17 +1,29 @@
 import crownIcon from "@/assets/crown.svg";
 import arrowRightIcon from "@/assets/arrow_right.svg";
 import Image from "next/image";
+import { dashboardColoMap } from "@/utils/dashboardColorMap";
 
-export function DashboardNameCard() {
-  const exampleColor = "bg-green_7AC555";
+type DashboardNameCardProps = {
+  name: string;
+  isOwner: boolean;
+  bgColor: string;
+};
+
+export function DashboardNameCard({
+  name,
+  isOwner,
+  bgColor,
+}: DashboardNameCardProps) {
   return (
-    <button className="w-full flex justify-between items-center border border-gray_D9D9D9 rounded-[8px] px-[20px] py-[17px] md:py-[22px]">
+    <button className="w-full flex justify-between items-center border border-gray_D9D9D9 rounded-[8px] px-[20px] py-[17px] lg:py-[22px] bg-white_FFFFFF">
       <div className="flex items-center gap-x-[16px]">
-        <div className="rounded-full w-[8px] h-[8px] bg-green_7AC555"></div>
+        <div
+          className={`rounded-full w-[8px] h-[8px] ${dashboardColoMap[bgColor]}`}
+        ></div>
 
         <div className="flex gap-x-[8px] items-center">
-          <span className="text-md md:text-lg text-black_333236">비브리지</span>
-          <Image src={crownIcon} alt="주인" />
+          <span className="text-md md:text-lg text-black_333236">{name}</span>
+          {isOwner && <Image src={crownIcon} alt="주인" />}
         </div>
       </div>
       <Image src={arrowRightIcon} alt="이동" />
