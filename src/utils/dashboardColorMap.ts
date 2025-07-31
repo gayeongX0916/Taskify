1a8ad40200333fc0e638a5ee0a1c9d15d8b3106b
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-31 19:33:58 +0900

    [feat/#71] colorMap 함수 추가

---
+++ src/utils/dashboardColorMap.ts
@@ -0,0 +1,7 @@
+export const dashboardColoMap: Record<string, string> = {
+  green: "bg-green_7AC555",
+  purple: "bg-purple_760DDE",
+  orange: "bg-orange_FFA500",
+  blue: "bg-blue_76A5EA",
+  pink: "bg-pink_E876EA",
+};
