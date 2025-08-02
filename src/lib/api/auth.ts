92cb992c95b943f9a433292cdebf1f5f8d5fed53
gayeongX0916 <gayeongx0916@gmail.com>
2025-08-02 16:37:15 +0900

    [feat/#78] 주석 달기

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,7 +1,9 @@
 import { LoginType, PasswordChangeType } from "@/types/users";
 import { api } from "./axios";
 
+// 로그인
 export const postLogin = (data: LoginType) => api.post("/auth/login", data);
 
+// 비밀번호 변경
 export const putPasswordChange = (data: PasswordChangeType) =>
   api.put("/auth/password", data);
