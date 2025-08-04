45a35cf2668d37e3e1ef556bf5e37c9dec92d0f8
gayeongX0916 <gayeongx0916@gmail.com>
2025-08-04 14:46:51 +0900

    [feat/#88] 타입 추가

--- src/types/dashboards.ts
+++ src/types/dashboards.ts
@@ -7,7 +7,17 @@ export interface putDashboardType extends postDashboardType {
   dashboardId: number;
 }
 
-export interface deleteInviteDashboardType{
-  dashboardId:number;
-  invitationId:number;
+export interface deleteInviteDashboardType {
+  dashboardId: number;
+  invitationId: number;
+}
+
+export interface getDashboardListTYpe {
+  id: number;
+  title: string;
+  color: string;
+  createdAt:string;
+  updatedAt:string;
+  createdByMe: boolean;
+  userId: number;
 }
