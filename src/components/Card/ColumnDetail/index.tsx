d928ad3bfbdfd9742b41f46c95786c4726ed4508
gayeongX0916 <gayeongx0916@gmail.com>
2025-08-05 11:22:00 +0900

    [feat/#93] 컬럼 상세 카드 수정

--- src/components/Card/ColumnDetail/index.tsx
+++ src/components/Card/ColumnDetail/index.tsx
@@ -4,47 +4,74 @@ import Image from "next/image";
 import calendarIcon from "@/assets/calendar_icon.svg";
 import { Avatar } from "@/components/common/Avatar";
 import { TagList } from "@/components/common/TagList";
+import { useEffect, useState } from "react";
+import { getCardType } from "@/types/cards";
+import { getCardDetail } from "@/lib/api/cards";
+import { useToastStore } from "@/lib/stores/toast";
 
 type ColumnDetailCardProps = {
-  img?: string | undefined;
-  title: string;
-  tags: string[];
-  username: string;
-  date: Date;
+  cardId: number;
 };
 
-function CalendarDate({ className, date }: { className?: string; date: Date }) {
-  const formatted = date.toLocaleDateString("ko-KR", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  });
+function CalendarDate({
+  className,
+  date,
+}: {
+  className?: string;
+  date?: string;
+}) {
+  if (!date) return null;
 
   return (
     <time className={`flex gap-x-[4px] items-center ${className}`}>
       <Image src={calendarIcon} alt="달력" />
-      <span className="text-xs text-gray_787486">{formatted}</span>
+      <span className="text-xs text-gray_787486">{date}</span>
     </time>
   );
 }
 
-export function ColumnDetailCard({
-  img,
-  title,
-  tags,
-  username,
-  date,
-}: ColumnDetailCardProps) {
+export function ColumnDetailCard({ cardId }: ColumnDetailCardProps) {
+  const addToast = useToastStore.getState().addToast;
+  const [detail, setDetail] = useState<getCardType>({
+    id: 0,
+    columnId: 0,
+    title: "",
+    description: "",
+    imageUrl: "",
+    dueDate: "",
+    tags: [],
+    createdAt: "",
+    assignee: {
+      id: 0,
+      nickname: "",
+      profileImageUrl: "",
+    },
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getCardDetail({ cardId });
+        setDetail(res.data);
+      } catch (error) {
+        addToast("카드 상세 조회에 실패했습니다.");
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <article
       className={`rounded-[6px] px-[12px] pb-[5px] bg-white_FFFFFF border border-D9D9D9 flex flex-col gap-y-[4px] md:flex-row md:gap-x-[20px] md:px-[20px] lg:flex-col lg:gap-y-[16px]  ${
-        img ? "pt-[12px] md:py-[16px]" : "pt-[5px] md:py-[14px] lg:py-[16px]"
+        detail.imageUrl
+          ? "pt-[12px] md:py-[16px]"
+          : "pt-[5px] md:py-[14px] lg:py-[16px]"
       } `}
     >
-      {img && (
+      {detail.imageUrl && (
         <div className="flex justify-center items-center">
           <Image
-            src={img}
+            src={detail.imageUrl}
             alt="예시"
             width={260}
             height={150}
@@ -55,19 +82,27 @@ export function ColumnDetailCard({
 
       <section className="flex flex-col gap-y-[6px] md:gap-y-[10px] md:flex-[9]">
         <header>
-          <h3 className="text-md md:text-lg text-black_333236">{title}</h3>
+          <h3 className="text-md md:text-lg text-black_333236">
+            {detail.title}
+          </h3>
         </header>
 
         <div className="flex flex-col gap-y-[6px] md:flex-row md:gap-x-[18px] md:w-full md:justify-between lg:flex-col">
           <div className="flex md:gap-x-[16px] md:items-center">
-            <TagList tags={tags} className="gap-y-[6px] gap-x-[6px]" />
+            <TagList tags={detail.tags} className="gap-y-[6px] gap-x-[6px]" />
 
-            <CalendarDate className="hidden md:flex lg:hidden" date={date} />
+            <CalendarDate
+              className="hidden md:flex lg:hidden"
+              date={detail.createdAt}
+            />
           </div>
 
           <div className="flex justify-between items-center">
-            <CalendarDate className="md:hidden lg:flex" date={date} />
-            <Avatar username={username} />
+            <CalendarDate
+              className="md:hidden lg:flex"
+              date={detail.createdAt}
+            />
+            <Avatar username={detail.assignee.nickname} />
           </div>
         </div>
       </section>
--- src/components/modal/Base/EditColumnModal.tsx
+++ src/components/modal/Base/EditColumnModal.tsx
@@ -2,7 +2,15 @@ import { ModalProps } from "@/types/ModalProps";
 import { BaseModal } from ".";
 import { ModalButton } from "@/components/common/Button/ModalButton";
 
-export function EditColumnModal({ isOpen, onClose }: ModalProps) {
+interface EditColumnModalProps extends ModalProps {
+  onDelete: () => void;
+}
+
+export function EditColumnModal({
+  isOpen,
+  onClose,
+  onDelete,
+}: EditColumnModalProps) {
   return (
     <BaseModal isOpen={isOpen} onClose={onClose} title="컬럼 관리">
       <div className="flex flex-col gap-y-[8px] mb-[24px]">
@@ -10,7 +18,7 @@ export function EditColumnModal({ isOpen, onClose }: ModalProps) {
         <input className="px-[16px] py-[15px] rounded-[8px] border border-gray_D9D9D9" />
       </div>
       <div className="flex gap-x-[7px]">
-        <ModalButton mode="cancel" onClick={onClose}>
+        <ModalButton mode="cancel" onClick={onDelete}>
           삭제
         </ModalButton>
         <ModalButton mode="any">변경</ModalButton>
