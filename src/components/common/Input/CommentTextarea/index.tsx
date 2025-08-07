54998feb6e360bee35281d5286bd83362d54d60d
gayeongX0916 <gayeongx0916@gmail.com>
2025-08-07 12:22:24 +0900

    [feat/#101] 댓글 추가

--- src/components/common/Input/CommentTextarea/index.tsx
+++ src/components/common/Input/CommentTextarea/index.tsx
@@ -1,10 +1,33 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import { ActionButton } from "../../Button/ActionButton";
+import { useCommentStore } from "@/lib/stores/comment";
+import { useToastStore } from "@/lib/stores/toast";
+import { postComment } from "@/lib/api/comments";
+import { postCommentType } from "@/types/comments";
+import { useParams } from "next/navigation";
 
-export function CommentTextarea() {
+type CommentTextareaProps = {
+  cardId: number;
+  columnId: number;
+};
+
+export function CommentTextarea({ cardId, columnId }: CommentTextareaProps) {
   const [value, setValue] = useState("");
+  const addComment = useCommentStore((state) => state.addComment);
+  const addToast = useToastStore.getState().addToast;
+  const { dashboardId } = useParams();
+
+  const handleAddComment = async (data: postCommentType) => {
+    try {
+      const res = await postComment(data);
+      addComment(cardId, res.data);
+      setValue("");
+    } catch (error) {
+      addToast("댓글 생성에 실패했습니다.");
+    }
+  };
 
   return (
     <div className="flex flex-col gap-y-[4px]">
@@ -16,7 +39,17 @@ export function CommentTextarea() {
           placeholder="댓글 작성하기"
           className="w-full px-[12px] py-[12px] rounded-[6px] border border-gray_D9D9D9 resize-none text-md text-black_171717 focus:outline-none focus:border-violet_5534DA lg:px-[16px] lg:py-[16px]"
         />
-        <ActionButton className="absolute right-[12px] bottom-[12px]">
+        <ActionButton
+          className="absolute right-[12px] bottom-[12px]"
+          onClick={() =>
+            handleAddComment({
+              cardId: cardId,
+              columnId: columnId,
+              dashboardId: Number(dashboardId),
+              content: value,
+            })
+          }
+        >
           입력
         </ActionButton>
       </div>
