f93c584c2f541f6ef38ff3afbd066baf208ff588
gayeongX0916 <gayeongx0916@gmail.com>
2025-08-08 21:49:35 +0900

    [feat#107] BaseInput 수정

--- src/components/common/Input/BaseInput/index.tsx
+++ src/components/common/Input/BaseInput/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type DefaultInputProps = {
   label: string;
@@ -8,6 +8,8 @@ type DefaultInputProps = {
   value?: string;
   onChange?: (value: string) => void;
   mode: "any" | "profile";
+  readonly?: boolean;
+  isTyping?: boolean;
 };
 
 export function BaseInput({
@@ -16,11 +18,10 @@ export function BaseInput({
   value,
   onChange,
   mode,
+  readonly,
+  isTyping,
 }: DefaultInputProps) {
-  const [isTyping, setIsTyping] = useState(false);
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsTyping(true);
     onChange?.(e.target.value);
   };
 
@@ -29,11 +30,16 @@ export function BaseInput({
       <span className="text-md text-black_333236 md:text-lg">{label}</span>
       <input
         className={`px-[16px] py-[12px] rounded-[8px] border border-gray_D9D9D9 text-lg w-full ${
-          mode==='profile' ? !isTyping && value ? "text-gray_9FA6B2" : "text-black_333236" : "text-black_333236"
+          mode === "profile"
+            ? !isTyping && value
+              ? "text-gray_9FA6B2"
+              : "text-black_333236"
+            : "text-black_333236"
         }`}
         placeholder={placeholder}
         value={value}
         onChange={handleChange}
+        readOnly={readonly}
       />
     </div>
   );
