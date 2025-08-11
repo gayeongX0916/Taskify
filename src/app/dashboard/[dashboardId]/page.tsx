5fd01d1ec4daa1f38e86a133c9b20dab349bedd3
gayeongX0916 <gayeongx0916@gmail.com>
2025-08-11 13:15:26 +0900

    [feat/#103] dashboardId 페이지 로딩 상태

--- src/app/dashboard/[dashboardId]/page.tsx
+++ src/app/dashboard/[dashboardId]/page.tsx
@@ -2,20 +2,22 @@
 
 import { ColumnCard } from "@/components/Card/Column";
 import { AddButton } from "@/components/common/Button/AddButton";
+import { Spinner } from "@/components/common/Spinner";
 import { CreateColumnModal } from "@/components/Modal/CreateColumn";
 import { getColumnList } from "@/lib/api/columns";
 import { useColumnStore } from "@/lib/stores/column";
+import { useLoadingStore } from "@/lib/stores/loading";
 import { useToastStore } from "@/lib/stores/toast";
-import { getColumnListType } from "@/types/columns";
+import { isAxiosError } from "axios";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const dashboardPage = () => {
   const addToast = useToastStore.getState().addToast;
-  const [isOpen, setIsOpen] = useState(false);
   const { dashboardId } = useParams();
   const dashboardIdNum = Number(dashboardId);
-
+  const { isLoading, startLoading, stopLoading } = useLoadingStore();
+  const [isOpen, setIsOpen] = useState(false);
   const columnsByDashboard = useColumnStore(
     (state) => state.columnsByDashboard?.[dashboardIdNum]
   );
@@ -23,16 +25,23 @@ const dashboardPage = () => {
     ? Object.values(columnsByDashboard)
     : [];
   const setColumnList = useColumnStore((state) => state.setColumnList);
-  // const {isLoading,startLoading,stopLoading}=useLoadingStore();
-
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        startLoading();
         const res = await getColumnList({ dashboardId: dashboardIdNum });
         setColumnList(dashboardIdNum, res.data.data);
       } catch (error) {
-        addToast("컬럼 목록 조회에 실패했습니다.");
+        if (isAxiosError(error)) {
+          addToast(
+            error.response?.data.message || "컬럼 목록 조회에 실패했습니다."
+          );
+        } else {
+          addToast("알 수 없는 오류가 발생했습니다.");
+        }
+      } finally {
+        stopLoading();
       }
     };
     fetchData();
@@ -42,18 +51,28 @@ const dashboardPage = () => {
     <main className="bg-gray_FAFAFA min-h-screen">
       <CreateColumnModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
       <div className="flex flex-col pb-[20px] lg:flex-row lg:overflow-x-auto">
-        <div className="flex flex-col lg:flex-row">
-          {columnArray.map(({ id, title }) => (
-            <ColumnCard key={id} columnId={id} title={title} />
-          ))}
+        <div className="flex flex-col lg:flex-row w-full">
+          {isLoading ? (
+            <div className="flex w-full justify-center pt-[100px]">
+              <Spinner />
+            </div>
+          ) : (
+            columnArray.map(({ id, title }) => (
+              <ColumnCard key={id} columnId={id} title={title} />
+            ))
+          )}
         </div>
-        <div className="px-[12px] pt-[16px] pb-[49px] md:py-[20px] md:px-[20px] lg:pt-[68px] lg:pb[0] lg:pl-[20px] lg:pr-[100px]">
-          <AddButton
-            mode="column"
-            className="w-full font-bold py-[20px] lg:w-[354px]"
-            onClick={() => setIsOpen(true)}
-          />
-        </div>
+
+        {!isLoading && (
+          <div className="px-[12px] pt-[16px] pb-[49px] md:py-[20px] md:px-[20px] lg:pt-[68px] lg:pb[0] lg:pl-[20px] lg:pr-[100px]">
+            <AddButton
+              mode="column"
+              className="w-full font-bold py-[20px] lg:w-[354px]"
+              onClick={() => setIsOpen(true)}
+              disabled={isLoading}
+            />
+          </div>
+        )}
       </div>
     </main>
   );
